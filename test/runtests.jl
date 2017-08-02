4c812755ba3ce164e1fea58831512c0cf04c493a
Tamas Nagy <tamas@tamasnagy.com>
2017-08-01 19:21:55 -0700

    add basic tests, fixes #2

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,33 @@
 using OMETIFF
+using Images
+using AxisArrays
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+@testset "Small OME-TIFFs" begin
+    @testset "Single Channel OME-TIFF" begin
+        img = OMETIFF.loadtiff("testdata/tiffs/single-channel.ome.tif")
+        @test size(img) == (167, 439)
+    end
+    @testset "Multi Channel OME-TIFF" begin
+        img = OMETIFF.loadtiff("testdata/tiffs/multi-channel.ome.tif")
+        @test size(img) == (167, 439, 3)
+        # check channel indexing
+        @test size(img[Axis{:channel}(:C1)]) == (167, 439)
+    end
+    @testset "Multi Channel Time Series OME-TIFF" begin
+        img = OMETIFF.loadtiff("testdata/tiffs/multi-channel-time-series.ome.tif")
+        @test size(img) == (167, 439, 3, 7)
+        # check channel indexing
+        @test size(img[Axis{:channel}(:C1)]) == (167, 439, 7)
+        # check time indexing
+        @test size(img[Axis{:time}(1)]) == (167, 439, 3)
+    end
+    @testset "Multi Channel Time Series OME-TIFF" begin
+        img = OMETIFF.loadtiff("testdata/tiffs/multi-channel-z-series.ome.tif")
+        @test size(img) == (167, 439, 5, 3)
+        # check channel indexing
+        @test size(img[Axis{:channel}(:C1)]) == (167, 439, 5)
+        # check z indexing
+        @test size(img[Axis{:z}(1)]) == (167, 439, 3)
+    end
+end
