ed220d04e21018252da7f3bd103caa501c2a2f54
Tamas Nagy <tamas@tamasnagy.com>
2017-09-19 14:30:55 -0700

    add new tests for multiple file loading

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,14 +5,14 @@ using Base.Test
 
 @testset "Single file OME-TIFFs" begin
     @testset "Single Channel OME-TIFF" begin
-        open("testdata/tiffs/single-channel.ome.tif") do f
+        open("testdata/singles/single-channel.ome.tif") do f
             s = Stream(format"OMETIFF", f, OMETIFF.extract_filename(f))
             img = OMETIFF.load(s)
             @test size(img) == (167, 439)
         end
     end
     @testset "Multi Channel OME-TIFF" begin
-        open("testdata/tiffs/multi-channel.ome.tif") do f
+        open("testdata/singles/multi-channel.ome.tif") do f
             s = Stream(format"OMETIFF", f, OMETIFF.extract_filename(f))
             img = OMETIFF.load(s)
             @test size(img) == (167, 439, 3)
@@ -21,7 +21,7 @@ using Base.Test
         end
     end
     @testset "Multi Channel Time Series OME-TIFF" begin
-        open("testdata/tiffs/multi-channel-time-series.ome.tif") do f
+        open("testdata/singles/multi-channel-time-series.ome.tif") do f
             s = Stream(format"OMETIFF", f, OMETIFF.extract_filename(f))
             img = OMETIFF.load(s)
             @test size(img) == (167, 439, 3, 7)
@@ -31,8 +31,8 @@ using Base.Test
             @test size(img[Axis{:time}(1)]) == (167, 439, 3)
         end
     end
-    @testset "Multi Channel Time Series OME-TIFF" begin
-        open("testdata/tiffs/multi-channel-z-series.ome.tif") do f
+    @testset "Multi Channel Z Series OME-TIFF" begin
+        open("testdata/singles/multi-channel-z-series.ome.tif") do f
             s = Stream(format"OMETIFF", f, OMETIFF.extract_filename(f))
             img = OMETIFF.load(s)
             @test size(img) == (167, 439, 5, 3)
@@ -40,6 +40,33 @@ using Base.Test
             @test size(img[Axis{:channel}(:C1)]) == (167, 439, 5)
             # check z indexing
             @test size(img[Axis{:z}(1)]) == (167, 439, 3)
+        end
+    end
+end
+@testset "Multi file OME-TIFFs" begin
+    @testset "Multi file Z stack with master file" begin
+        # load the master file that contains the full OME-XML
+        @testset "Load master file)" begin
+            open("testdata/multiples/master/multifile-Z1.ome.tiff") do f
+                s = Stream(format"OMETIFF", f, OMETIFF.extract_filename(f))
+                img = OMETIFF.load(s)
+                @test size(img) == (24, 18, 5)
+            end
+        end
+        # load the secondary file that only has a pointer to the full OME-XML
+        @testset "Load secondary file" begin
+            open("testdata/multiples/master/multifile-Z2.ome.tiff") do f
+                s = Stream(format"OMETIFF", f, OMETIFF.extract_filename(f))
+                img = OMETIFF.load(s)
+                @test size(img) == (24, 18, 5)
+            end
+        end
+    end
+    @testset "Multi file Z stack with OME companion file" begin
+        open("testdata/multiples/companion/multifile-Z1.ome.tiff") do f
+            s = Stream(format"OMETIFF", f, OMETIFF.extract_filename(f))
+            img = OMETIFF.load(s)
+            @test size(img) == (24, 18, 5)
         end
     end
 end
